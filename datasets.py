1488c4492b4b269228ce4dbce0667ea09d2e55b3
OrrAvrech <orravrech@gmail.com>
2022-01-14 07:21:27 +0200

    adding Imagenette datasets, training script is done

---
+++ datasets.py
@@ -0,0 +1,51 @@
+from PIL import Image
+from torch.utils.data import Dataset
+from fastai.vision.all import untar_data, URLs, get_image_files, parent_label
+
+
+class Imagenette(Dataset):
+
+    lbl_dict = dict(
+        n01440764='tench',
+        n02102040='English springer',
+        n02979186='cassette player',
+        n03000684='chain saw',
+        n03028079='church',
+        n03394916='French horn',
+        n03417042='garbage truck',
+        n03425413='gas pump',
+        n03445777='golf ball',
+        n03888257='parachute'
+    )
+
+    def __init__(self, split='train', transform=None):
+        path = untar_data(URLs.IMAGENETTE_160)
+        self.file_names = get_image_files(path, folders=split)
+        self.transform = transform
+        self.split = split
+
+    def __len__(self):
+        return len(self.file_names)
+
+    def __getitem__(self, idx):
+        f_name = self.file_names[idx]
+        image = Image.open(f_name)
+        label = self.get_label_dict()[parent_label(f_name)]
+        return image, label
+
+    @classmethod
+    def get_label_dict(cls):
+        return cls.lbl_dict
+
+
+class ImagenettePair(Imagenette):
+    def __getitem__(self, idx):
+        f_name = self.file_names[idx]
+        image = Image.open(f_name)
+        image_1 = image
+        image_2 = image
+        if self.transform is not None:
+            image_1 = self.transform(image)
+            image_2 = self.transform(image)
+
+        return image_1, image_2
